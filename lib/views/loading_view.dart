d4ef9c3f033768cd57ed59f64f5823950ad8dbb3
wonhj12 <wonhj12@naver.com>
2025-03-22 11:51:17 +0900

    feat: 로딩 화면 구현

---
+++ lib/views/loading_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:yeogijeogi/components/common/custom_scaffold.dart';
+import 'package:yeogijeogi/utils/palette.dart';
+import 'package:yeogijeogi/view_models/loading_view_model.dart';
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final LoadingViewModel loadingViewModel = context.watch<LoadingViewModel>();
+
+    return CustomScaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Lottie.asset('assets/animations/loading.json'),
+            SizedBox(height: 40.h),
+
+            Text('산책 코스를 찾고 있어요!', style: Palette.title),
+            SizedBox(height: 8.h),
+
+            Text('잠시만 기다려 주세요.', style: Palette.headline),
+          ],
+        ),
+      ),
+    );
+  }
+}
