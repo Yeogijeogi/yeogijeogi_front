b5cd2a7993a527a0340d6baa7fcf7e053fa34b92
wonhj12 <wonhj12@naver.com>
2025-03-22 11:59:20 +0900

    desing: CustomScaffold에 screenutils 적용

--- lib/components/common/custom_scaffold.dart
+++ lib/components/common/custom_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:yeogijeogi/components/common/loading_screen.dart';
 
@@ -59,7 +60,7 @@ class CustomScaffold extends StatelessWidget {
             children: [
               // body
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
                 child: body,
               ),
 
