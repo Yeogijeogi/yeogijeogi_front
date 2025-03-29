0603cbbb029a72f22e181ebcac2c5761db89e8d5
wonhj12 <wonhj12@naver.com>
2025-03-29 11:07:40 +0900

    feat: 산책 종료 버튼 클릭시 종료 확인 다이얼로그 표시 로직 추가

--- lib/view_models/walk/walk_view_model.dart
+++ lib/view_models/walk/walk_view_model.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yeogijeogi/components/walk/walk_end_dialog.dart';
 
 class WalkViewModel with ChangeNotifier {
   BuildContext context;
 
   WalkViewModel({required this.context});
+
+  void onTapEnd() async {
+    await showWalkEndDialog(
+      context: context,
+      onTapSave: context.pop,
+      onTapCancel: context.pop,
+    );
+  }
 }
--- lib/views/walk/walk_view.dart
+++ lib/views/walk/walk_view.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:yeogijeogi/components/common/custom_button.dart';
 import 'package:yeogijeogi/components/common/custom_scaffold.dart';
+import 'package:yeogijeogi/view_models/walk/walk_view_model.dart';
 
 class WalkView extends StatelessWidget {
   const WalkView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final WalkViewModel walkViewModel = context.watch<WalkViewModel>();
+
     return CustomScaffold(
       hasPadding: false,
       useSafeArea: false,
@@ -21,7 +25,10 @@ class WalkView extends StatelessWidget {
             bottom: true,
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: CustomButton(text: '산책 종료하기', onTap: () {}),
+              child: CustomButton(
+                text: '산책 종료하기',
+                onTap: walkViewModel.onTapEnd,
+              ),
             ),
           ),
         ],
