8c3b0b4be7615339e220a649aac7c073d2379086
wonhj12 <wonhj12@naver.com>
2025-03-29 18:15:47 +0900

    mod: CustomTextButton에 텍스트 스타일 커스텀 가능하도록 수정

--- lib/components/common/custom_text_button.dart
+++ lib/components/common/custom_text_button.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:yeogijeogi/utils/palette.dart';
 
 class CustomTextButton extends StatelessWidget {
   /// 버튼 텍스트
   final String text;
 
+  /// 버튼 스타일
+  final TextStyle? style;
+
   /// 버튼 클릭시 실행 함수
   final Function()? onTap;
 
-  const CustomTextButton({super.key, this.onTap, required this.text});
+  const CustomTextButton({
+    super.key,
+    required this.text,
+    this.style,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
+    return TextButton(
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.all(8.w),
+        minimumSize: Size(0, 0),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        overlayColor: Palette.onSurfaceVariant,
+      ),
+      onPressed: onTap,
       child: Text(
         text,
-        style: Palette.caption.copyWith(color: Palette.onSurfaceVariant),
+        style:
+            style ?? Palette.caption.copyWith(color: Palette.onSurfaceVariant),
       ),
     );
   }
