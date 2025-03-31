3aa30fe630096134236a5e4cd7ae905955bd2dbf
wonhj12 <wonhj12@naver.com>
2025-03-31 14:15:21 +0900

    design: 슬라이더 틱 추가, 비활성 상태 ui 통일

--- lib/components/walk/slider_container.dart
+++ lib/components/walk/slider_container.dart
@@ -13,14 +13,14 @@ class SliderContainer extends StatelessWidget {
   final double value;
 
   /// 슬라이더 값 변경 함수
-  final Function(double) onChanged;
+  final Function(double)? onChanged;
 
   const SliderContainer({
     super.key,
     required this.title,
     required this.criteria,
     required this.value,
-    required this.onChanged,
+    this.onChanged,
   });
 
   @override
@@ -46,14 +46,20 @@ class SliderContainer extends StatelessWidget {
               trackHeight: 8.h,
               activeTrackColor: Palette.surfaceVariant,
               inactiveTrackColor: Palette.surfaceVariant,
+              disabledActiveTrackColor: Palette.surfaceVariant,
+              disabledInactiveTrackColor: Palette.surfaceVariant,
               thumbColor: Palette.primary,
+              disabledThumbColor: Palette.primary,
               thumbShape: RoundSliderThumbShape(enabledThumbRadius: 8.r),
               padding: EdgeInsets.zero,
+              tickMarkShape: SliderTickMarkShape.noTickMark,
+              trackShape: RectangularSliderTrackShape(),
             ),
             child: Slider(
               value: value,
               min: 0,
-              max: (criteria.length - 1).toDouble(),
+              max: 10,
+              divisions: 10,
               onChanged: onChanged,
             ),
           ),
