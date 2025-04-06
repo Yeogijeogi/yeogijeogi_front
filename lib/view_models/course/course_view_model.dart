aa7704332bfca3ad961ab85d7cf35cd321fb3c6b
wonhj12 <wonhj12@naver.com>
2025-04-07 08:44:53 +0900

    mod: 상세보기 페이지 관련 함수 추가

--- lib/view_models/course/course_view_model.dart
+++ lib/view_models/course/course_view_model.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yeogijeogi/components/common/custom_dialog.dart';
 import 'package:yeogijeogi/models/course_model.dart';
+import 'package:yeogijeogi/utils/enums/dialog_type.dart';
 
 class CourseViewModel with ChangeNotifier {
   final BuildContext context;
   final CourseModel courseModel;
+
+  // 메모 텍스트 컨트롤러
+  TextEditingController controller = TextEditingController();
 
   // 모달 시트 컨트롤러
   final DraggableScrollableController draggableController =
@@ -23,6 +29,23 @@ class CourseViewModel with ChangeNotifier {
       isExpanded = isNowExpanded;
       notifyListeners();
     }
+  }
+
+  /// 앱바 뒤로가기 버튼
+  void onTapBack() {
+    draggableController.animateTo(
+      0.228,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void onTapDelete() {
+    showCustomDialog(
+      type: DialogType.deleteCourse,
+      context: context,
+      onTapAction: context.pop,
+    );
   }
 
   @override
