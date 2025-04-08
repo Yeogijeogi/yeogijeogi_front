671540d65e0ae0afabcd2b4fdc4366232b3bf34f
dayoung20 <adask13@naver.com>
2025-04-08 15:01:18 +0900

    feat: walk model

--- lib/models/walk_model.dart
+++ lib/models/walk_model.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yeogijeogi/models/objects/coordinate.dart';
+import 'package:yeogijeogi/models/objects/recommendation.dart';
 
 class WalkModel with ChangeNotifier {
   /// 시작 주소
@@ -11,6 +13,7 @@ class WalkModel with ChangeNotifier {
   int? time;
 
   /// 평균 속도
+  double? averageSpeed;
 
   /// 거리
   double? distance;
@@ -23,4 +26,24 @@ class WalkModel with ChangeNotifier {
 
   /// 메모
   String? memo;
+
+  /// 추천 목적지 리스트
+  List<Recommendation>? recommendationList;
+
+  /// 경로 좌표 리스트
+  List<Coordinate>? courseList;
+
+  /// 모델 초기화
+  void reset() {
+    startName = null;
+    endName = null;
+    time = null;
+    averageSpeed = null;
+    distance = null;
+    mood = null;
+    difficulty = null;
+    memo = null;
+    recommendationList = null;
+    courseList = null;
+  }
 }
