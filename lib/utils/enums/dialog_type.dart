79f2ef1f8fb677c0969d86b8bca7dab7c546c436
wonhj12 <wonhj12@naver.com>
2025-04-12 10:28:37 +0900

    feat: 권한 다이얼로그 타입 추가 - 취소 버튼 제거 옵션 추가

--- lib/utils/enums/dialog_type.dart
+++ lib/utils/enums/dialog_type.dart
@@ -6,7 +6,12 @@ enum DialogType {
     action: '회원탈퇴',
   ),
   notSave(title: '저장하지 않고 종료할까요?', body: '코스 데이터가 저장되지 않고 사라져요.', action: '종료'),
-  deleteCourse(title: '코스를 삭제할까요?', body: '이 작업은 다시 되돌릴 수 없어요.', action: '삭제');
+  deleteCourse(title: '코스를 삭제할까요?', body: '이 작업은 다시 되돌릴 수 없어요.', action: '삭제'),
+  location(
+    title: '위치 권한을 허용해주세요.',
+    body: '앱을 사용하기 위해서는 위치 권한이 필요합니다.',
+    action: '설정',
+  );
 
   /// 제목 텍스트
   final String title;
