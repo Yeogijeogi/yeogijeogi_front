121f267ba6b9d403163044e29af0aac4a7dba438
wonhj12 <wonhj12@naver.com>
2025-04-21 20:10:03 +0900

    refactor: 코스 모달 컴포넌트 분리

--- lib/views/course/course_view.dart
+++ lib/views/course/course_view.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:yeogijeogi/components/common/custom_scaffold.dart';
-import 'package:yeogijeogi/components/walk/course_detail.dart';
-import 'package:yeogijeogi/utils/palette.dart';
+import 'package:yeogijeogi/components/course/course_modal.dart';
 import 'package:yeogijeogi/view_models/course/course_view_model.dart';
-import 'package:yeogijeogi/components/course/course_detail_view.dart';
 
 class CourseView extends StatelessWidget {
   const CourseView({super.key});
@@ -26,98 +23,13 @@ class CourseView extends StatelessWidget {
           const NaverMap(),
 
           // 모달
-          DraggableScrollableSheet(
-            controller: courseViewModel.draggableController,
-            maxChildSize: 1.0,
-            initialChildSize: 0.228,
-            minChildSize: 0.228,
-            snap: true,
-            snapSizes: const [0.228, 1.0],
-            builder: (context, scrollController) {
-              return Container(
-                decoration: BoxDecoration(
-                  color: Palette.surface,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(20.r),
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    // 핸들
-                    if (!courseViewModel.isExpanded)
-                      Container(
-                        width: 40.w,
-                        height: 4.h,
-                        margin: EdgeInsets.only(top: 16.h, bottom: 20.h),
-                        decoration: BoxDecoration(
-                          color: Palette.onSurfaceVariant,
-                          borderRadius: BorderRadius.circular(10.r),
-                        ),
-                      ),
-
-                    // 내용 영역
-                    Expanded(
-                      child: ListView(
-                        padding: EdgeInsets.zero,
-                        controller: scrollController,
-                        physics:
-                            courseViewModel.isExpanded
-                                ? null
-                                : ClampingScrollPhysics(),
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(
-                              bottom: 20.h,
-                              left: 20.w,
-                              right: 20.w,
-                            ),
-                            child:
-                                courseViewModel.isExpanded
-                                    ? CourseDetailView(
-                                      course:
-                                          courseViewModel.courseModel.course!,
-                                      controller: courseViewModel.controller,
-                                      onTapBack: courseViewModel.onTapBack,
-                                      onTapDelete: courseViewModel.onTapDelete,
-                                    )
-                                    : Align(
-                                      alignment: Alignment.topCenter,
-                                      child: CourseDetail(
-                                        name:
-                                            courseViewModel
-                                                .courseModel
-                                                .course!
-                                                .name,
-                                        address:
-                                            courseViewModel
-                                                .courseModel
-                                                .course!
-                                                .address,
-                                        distance:
-                                            courseViewModel
-                                                .courseModel
-                                                .course!
-                                                .distance,
-                                        distanceLabel: '이동 거리',
-                                        walk:
-                                            '${courseViewModel.courseModel.course!.speed}km/h',
-                                        walkLabel: '평균 속도',
-                                        time:
-                                            courseViewModel
-                                                .courseModel
-                                                .course!
-                                                .time,
-                                        timeLabel: '소요 시간',
-                                      ),
-                                    ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
+          CourseModal(
+            draggableScrollableController: courseViewModel.draggableController,
+            course: courseViewModel.courseModel.course,
+            isExpanded: courseViewModel.isExpanded,
+            textEditingController: courseViewModel.controller,
+            onTapBack: courseViewModel.onTapBack,
+            onTapDelete: courseViewModel.onTapDelete,
           ),
         ],
       ),
