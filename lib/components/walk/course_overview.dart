e6cd608d7b7e7c89241997a21603b43007c2ef82
wonhj12 <wonhj12@naver.com>
2025-05-06 22:23:49 +0900

    design: 지도 고정시키고 CourseDetail만 스크롤 되도록 수정

--- lib/components/walk/course_overview.dart
+++ lib/components/walk/course_overview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:yeogijeogi/components/walk/course_detail.dart';
 import 'package:yeogijeogi/models/objects/recommendation.dart';
 
@@ -11,58 +12,63 @@ class CourseOverview extends StatelessWidget {
 
   final Function(NaverMapController) onMapReady;
 
-  final Function(NCameraUpdateReason, bool) onCameraChange;
-
   /// 페이지 컨트롤러
   final PageController controller;
+
+  final Function(int)? onPageChanged;
 
   const CourseOverview({
     super.key,
     required this.recommendations,
-    required this.controller,
     required this.options,
     required this.onMapReady,
-    required this.onCameraChange,
+    required this.controller,
+    required this.onPageChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: PageView.builder(
-        controller: controller,
-        itemCount: 3,
-        physics: const BouncingScrollPhysics(
-          decelerationRate: ScrollDecelerationRate.fast,
-        ),
-        itemBuilder: (_, index) {
-          return FractionallySizedBox(
-            widthFactor: 1 / controller.viewportFraction,
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                NaverMap(
-                  options: options,
-                  onMapReady: onMapReady,
-                  onCameraChange: onCameraChange,
-                ),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          // 지도
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.r),
+            child: NaverMap(options: options, onMapReady: onMapReady),
+          ),
 
-                // 코스 세부 정보
-                CourseDetail(
-                  name: recommendations[index].name,
-                  address: recommendations[index].address,
-                  distance: recommendations[index].distance,
-                  distanceLabel: '거리',
-                  walk: '${recommendations[index].walks}',
-                  walkLabel: '걸음',
-                  time: recommendations[index].time,
-                  timeLabel: '시간',
-                  margin: true,
-                  shadow: true,
-                ),
-              ],
+          // 코스 정보
+          Container(
+            margin: EdgeInsets.only(bottom: 20.h),
+            height: 113.h,
+            child: PageView.builder(
+              controller: controller,
+              itemCount: 3,
+              physics: const BouncingScrollPhysics(
+                decelerationRate: ScrollDecelerationRate.fast,
+              ),
+              onPageChanged: onPageChanged,
+              itemBuilder: (_, index) {
+                return FractionallySizedBox(
+                  widthFactor: 1 / controller.viewportFraction,
+                  child: CourseDetail(
+                    name: recommendations[index].name,
+                    address: recommendations[index].address,
+                    distance: recommendations[index].distance,
+                    distanceLabel: '거리',
+                    walk: '${recommendations[index].walks}',
+                    walkLabel: '걸음',
+                    time: recommendations[index].time,
+                    timeLabel: '시간',
+                    margin: true,
+                    shadow: true,
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
