0102da954d78a11947d8b7acfddb90136e1044c0
wonhj12 <wonhj12@naver.com>
2025-05-06 23:39:10 +0900

    feat: 소수점 자리 수 지정하는 util 함수 구현

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,8 +1,13 @@
 /// 평균 속도 계산
 double calAvgSpeed(double? distance, int? time) {
   if (time != null && time != 0 && distance != null) {
-    return distance / time;
+    return toFixedDigits(distance / time);
   } else {
     return 0;
   }
+}
+
+/// 소수점 자리를 지정해서 반환하는 함수
+double toFixedDigits(double value, {int digit = 2}) {
+  return double.parse(value.toStringAsFixed(digit));
 }
